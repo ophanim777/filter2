4617ebc20bc99ce45cd62852b5a0e2d33a033904
ophanim777 <rikyjo49@gmail.com>
2025-07-27 11:07:52 +0700

    add remove outside interval

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
-for (let i = 0; i < arr.length; i++)
+for (let i = 0; i < arr.length; i++){
+    let val = arr[i];
+
+    // remove if outside of the interval
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+}
 
 let arr = [5, 3, 8, 1];
 
