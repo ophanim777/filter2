763232bcf8b4eae55939ee6d66b3ccea819947dd
ophanim777 <rikyjo49@gmail.com>
2025-07-27 11:09:33 +0700

    add function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
+function filterRangeInPlace(arr, a, b) {
+
 for (let i = 0; i < arr.length; i++){
     let val = arr[i];
 
-    // remove if outside of the interval
     if (val < a || val > b) {
       arr.splice(i, 1);
       i--;
     }
+}
 }
 
 let arr = [5, 3, 8, 1];
